f75c5f387cf73968cf0026028cc7a4f7f525a294
Barry-B-Benson6 <62171092+Barry-B-Benson6@users.noreply.github.com>
2023-03-19 13:10:23 +1030

    Fixed permissions to entity properties and added isDirty

--- WinShooter/Entity.vb
+++ WinShooter/Entity.vb
@@ -1,75 +1,101 @@
 ﻿Public MustInherit Class Entity
 
-    Public Property IsAlive As Integer
+    Private _IsAlive As Boolean
+    Public Property IsAlive As Boolean
         Get
-            Return Nothing
+            Return _IsAlive
         End Get
-        Set(value As Integer)
+        Protected Set(value As Boolean)
+            _IsAlive = value
         End Set
     End Property
 
+    Private _Position As GamePosition
     Public Property Position As GamePosition
         Get
             SyncLock Me
-                Return Nothing
+                Return _Position
             End SyncLock
         End Get
-        Set(value As GamePosition)
+        Private Set(value As GamePosition)
             SyncLock Me
-
+                _Position = value
             End SyncLock
         End Set
     End Property
 
+    Private _Game As WinShooter.Game
     Public Property Game As WinShooter.Game
         Get
-            Return Nothing
+            Return _Game
         End Get
-        Set(value As WinShooter.Game)
+        Protected Set(value As WinShooter.Game)
+            _Game = value
         End Set
     End Property
 
+    Private _EntityId As Guid
     Public Property EntityId As Guid
         Get
-            Return Nothing
+            Return _EntityId
         End Get
-        Set(value As Guid)
+        Protected Set(value As Guid)
+            _EntityId = value
         End Set
     End Property
 
-    Public Property Name As Integer
+    Private _Name As String
+    Public Property Name As String
         Get
-            Return Nothing
+            Return _Name
         End Get
-        Set(value As Integer)
+        Protected Set(value As String)
+            _Name = value
         End Set
     End Property
 
-    Public Overridable ReadOnly Property LocallyOwned As Boolean
+    Private _LocallyOwned As Boolean
+    Public Property LocallyOwned As Boolean
         Get
-            Return Nothing
+            Return _LocallyOwned
         End Get
+        Protected Set(value As Boolean)
+            _LocallyOwned = LocallyOwned
+        End Set
     End Property
 
+    Private _Motion As WinShooter.Motion
     Public Property Motion As WinShooter.Motion
         Get
             SyncLock Me
-                Return Nothing
+                Return _Motion
             End SyncLock
         End Get
         Set(value As WinShooter.Motion)
             SyncLock Me
+                _Motion = value
+            End SyncLock
+        End Set
+    End Property
 
+    Private _isDirty As Boolean
+    Public Property isDirty As Boolean
+        Get
+            SyncLock Me
+                Return _isDirty
+            End SyncLock
+        End Get
+        Protected Set(value As Boolean)
+            SyncLock Me
+                _isDirty = value
             End SyncLock
         End Set
     End Property
 
     ''' <remarks></remarks>
-    Public Overridable Sub UpdateState()
-
-    End Sub
+    Public MustOverride Sub UpdateState()
 
     Public Sub UpdatePosition()
-
+        Position = Motion.CalculatePositionAtTime(DateTime.Now)
     End Sub
 End Class
