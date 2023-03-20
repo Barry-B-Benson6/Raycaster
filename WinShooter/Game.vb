3f080f92d26a37d55601cf294265a18b87ac64f4
Barry-B-Benson6 <josh2chapman@gmail.com>
2023-03-20 15:12:08 +1030

    Added Threads for rendering cycle and state cycle

--- WinShooter/Game.vb
+++ WinShooter/Game.vb
@@ -1,7 +1,10 @@
-﻿Public Class Game
+﻿Imports System.Threading
+Public Class Game
 
-    Public Sub New(initialPositionPLayer As GamePosition, map As GameMap, fov As Integer, resQuality As Integer)
+    Public Sub New(initialPositionPLayer As GamePosition, map As GameMap, fov As Integer, resQuality As Integer, form As Form)
 
+        AddHandler form.Paint, AddressOf Render
+        Me.Form = form
         Dim ClientPlayer = New Player("Cinnabun", New Motion(initialPositionPLayer, New GameVelocity(0, 0, 0), DateTime.UtcNow), Me, True)
         AddEntity(ClientPlayer)
         Me.Map = map
@@ -16,6 +19,8 @@
         Next
         Me.Sight = New Renderer(HUD, rays, ClientPlayer, Me)
     End Sub
+
+    Private ReadOnly Form As Form
 
     Dim _Entities As Dictionary(Of Guid, Entity)
     Public Property Entities As Dictionary(Of Guid, Entity)
@@ -34,7 +39,29 @@
     Public Property InputState As New InputState()
 
     Public Sub Start()
+        Dim StateCycle As Thread = New Thread(AddressOf funcStateCycle)
+        Dim RenderCycle As Thread = New Thread(AddressOf funcRenderCycle)
+        StateCycle.Start()
+        RenderCycle.Start()
+    End Sub
 
+    Private Sub funcStateCycle()
+        While True
+            For i = 0 To Entities.Count - 1
+                Entities.Values(i).UpdateState(DateTime.UtcNow)
+                Entities.Values(i).UpdatePosition(DateTime.UtcNow)
+            Next
+        End While
+    End Sub
+
+    Private Sub funcRenderCycle()
+        While True
+            Form.Invoke(Sub() Form.Refresh())
+        End While
+    End Sub
+
+    Private Sub Render(sender As Form, e As PaintEventArgs)
+        Sight.Render(e, sender.Size)
     End Sub
 
     Public Sub AddEntity(entity As Entity)
