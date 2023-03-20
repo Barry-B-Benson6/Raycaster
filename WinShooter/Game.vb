838c35b73d7396ca45463be2c94c9fad5e114023
Barry-B-Benson6 <josh2chapman@gmail.com>
2023-03-20 14:57:37 +1030

    Made game constructor and addEntity function

--- WinShooter/Game.vb
+++ WinShooter/Game.vb
@@ -1,44 +1,43 @@
 ﻿Public Class Game
 
-    Dim _Entities As List(Of Entity)
-    Public Property Entities As System.Collections.Generic.List(Of WinShooter.Entity)
+    Public Sub New(initialPositionPLayer As GamePosition, map As GameMap, fov As Integer, resQuality As Integer)
+
+        Dim ClientPlayer = New Player("Cinnabun", New Motion(initialPositionPLayer, New GameVelocity(0, 0, 0), DateTime.UtcNow), Me, True)
+        AddEntity(ClientPlayer)
+        Me.Map = map
+
+        Dim HUD = New HUD(ClientPlayer.EntityId, Me)
+
+        Dim rays As New List(Of Ray)
+        For i = 0 To resQuality - 1
+            Dim diff = -fov / 2 + i / (resQuality / fov)
+            Dim ray = New Ray(diff)
+            rays.Add(ray)
+        Next
+        Me.Sight = New Renderer(HUD, rays, ClientPlayer, Me)
+    End Sub
+
+    Dim _Entities As Dictionary(Of Guid, Entity)
+    Public Property Entities As Dictionary(Of Guid, Entity)
         Get
             Return _Entities
         End Get
-        Private Set(value As System.Collections.Generic.List(Of WinShooter.Entity))
+        Private Set(value As Dictionary(Of Guid, Entity))
             _Entities = value
         End Set
     End Property
 
-    Public Overridable Property Map As GameMap
-        Get
-            Return Nothing
-        End Get
-        Set(value As GameMap)
-        End Set
-    End Property
+    Public Property Map As GameMap
 
-    Public Property Sight As Renderer
-        Get
-            Return Nothing
-        End Get
-        Set(value As Renderer)
-        End Set
-    End Property
+    Public ReadOnly Property Sight As Renderer
 
-    Public Property InputState As InputState
-        Get
-            Return Nothing
-        End Get
-        Set(value As InputState)
-        End Set
-    End Property
+    Public Property InputState As New InputState()
 
     Public Sub Start()
 
     End Sub
 
     Public Sub AddEntity(entity As Entity)
-
+        Entities.Add(entity.EntityId, entity)
     End Sub
 End Class
